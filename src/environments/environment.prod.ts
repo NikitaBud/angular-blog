e5998d848aa164521a0e6884a7d4b2f97a713fe4
Nikita Budiakov <budyakov91@gmail.com>
2020-01-15 21:05:10 +0100

    Add base of firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyDWh3TRQNh8EZgem6KFEzxlaKkuUUmg3OE'
+  apiKey: 'AIzaSyDWh3TRQNh8EZgem6KFEzxlaKkuUUmg3OE',
+  fbDbUrl: 'https://angular-blog-12116.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyDWh3TRQNh8EZgem6KFEzxlaKkuUUmg3OE'
+  apiKey: 'AIzaSyDWh3TRQNh8EZgem6KFEzxlaKkuUUmg3OE',
+  fbDbUrl: 'https://angular-blog-12116.firebaseio.com'
 };
 
 /*
